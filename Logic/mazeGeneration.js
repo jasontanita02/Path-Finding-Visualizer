03a8595155b06f1562ee3f8b67f1c5c06d23c1e8
JasonTanita <jasonhtanita@gmail.com>
2025-11-20 10:22:04 -0800

    fixed key entries, now usable for pathing

--- Logic/mazeGeneration.js
+++ Logic/mazeGeneration.js
@@ -1,4 +1,4 @@
-
+import dfs from './pathing.js'
 import shuffle from './shuffle.js'
 
 
@@ -12,9 +12,9 @@ function generateMaze(col, row){
     }
     return maze
 }
-const maze = generateMaze(4,4);
+const maze = generateMaze(2,2);
 
-
+let graph = new Map();
 
 function traverseMaze(maze, row, col, maxCol, maxRow, graph = new Map()){
     //Calculate the neighbors
@@ -24,17 +24,14 @@ function traverseMaze(maze, row, col, maxCol, maxRow, graph = new Map()){
     let W = col - 1; 
     let numberLetter = {"N": N, "E": E, "S": S, "W": W};
     let neighbors = ["N", "E", "S", "W"];
-    let randomDirections = shuffle(neighbors);
+    let randomDirections = (neighbors);
 
     //Iterate through the neighbors
     
     //Visited
     maze[row][col] = 1;
     
-    let x = [row,col];
-    // if (graph.has(x) == true){
-    //     console.log(graph);
-    // }
+    let x = `[${row},${col}]`;
     graph.set(x, []);
     
     // console.log(graph);
@@ -58,8 +55,8 @@ function traverseMaze(maze, row, col, maxCol, maxRow, graph = new Map()){
 
             // If we made it here its in bounds and hasn't been visited
             //Add that connection
-            graph.get(x).push([newRow,col]);
-            console.log(graph);
+            graph.get(x).push(`[${newRow},${col}]`);
+            // console.log(graph);
 
             
             traverseMaze(maze, newRow, col, maxCol, maxRow, graph);
@@ -76,15 +73,38 @@ function traverseMaze(maze, row, col, maxCol, maxRow, graph = new Map()){
                 continue;
             }
             // If we made it here its in bounds and hasn't been visited
+            
+            
+            graph.get(x).push(`[${row},${newCol}]`);
 
-            graph.get(x).push([row,newCol]);
-
-            console.log(graph);
             traverseMaze(maze, row, newCol, maxCol, maxRow, graph);
         }
     }
 }
  
-traverseMaze(maze, 0, 0, maze[0].length, maze.length);
+traverseMaze(maze, 0, 0, maze[0].length, maze.length, graph);
 
 
+console.log("After everything");
+// console.log(graph);
+
+graph.get("[0,0]").push("[1,0]");
+graph.get("[0,0]").push("[1,1]");
+
+console.log(graph)
+
+for (let i = 0; i < graph.get("[0,0]").length; i++){
+    let x = graph.get("[0,0]")[i];
+    console.log(x);
+    console.log(graph.get(x))
+}
+console.log(graph.get("[0,0]").length);
+console.log(graph.get("[0,0]"));
+
+
+console.log(graph.get("0,0"));
+
+
+
+// dfs(graph, [0,0], "hello");
+//TODO make bidirectional in graph. Easy fix.
